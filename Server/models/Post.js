c8298a1faa4dcc43db9f78d3c0492389798410fe
dimodimow <ivanov.dimo0029@gmail.com>
2023-02-24 18:08:06 +0200

    remove unnecesarry code

--- Server/models/Post.js
+++ Server/models/Post.js
@@ -10,16 +10,3 @@ const postSchema = new mongoose.Schema({
     required: true,
   },
 });
-
-// public string? Description { get; set; }
-
-// public byte[] Content { get; set; }
-
-// public PostType Type { get; set; }
-
-// public virtual ICollection<PostLikeEntity> PostLikes { get; set; }
-
-// public virtual ICollection<CommentEntity> Comments { get; set; }
-
-// [NotMapped]
-// public int Likes => this.PostLikes.Count;
