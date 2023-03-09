6f4c79ebd7ae012d408292580c3580541024f24b
dimodimow <ivanov.dimo0029@gmail.com>
2023-03-10 01:18:20 +0200

    add tables seeder to the db

--- Server/db/TableCreation.ts
+++ Server/db/TableCreation.ts
@@ -1,12 +1,17 @@
 import pool from "./db.js";
 
-const IdColumn = "Id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY"
+const UuidQuery: string = "UUID DEFAULT uuid_generate_v4()";
+const IdColumn: string = `Id ${UuidQuery} NOT NULL PRIMARY KEY`;
+const TrackableColumns: string = `CreatedAt date, ModifiedAt date`;
+const UserIdRefence: string = `UserId ${UuidQuery} references Users(Id) NOT NULL`;
 
-const foo = async () => {
+const foo = async (): Promise<void> => {
   await pool.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   await pool.query(
-    `CREATE TABLE Users(${IdColumn}, Firstname text NOT NULL, Lastname text NOT NULL, Username text NOT NULL, Age smallint NOT NULL)` +
-    `CREATE TABLE Posts(${IdColumn}, ContentUrl text, Description varchar(200))`
+    `CREATE TABLE Users(${IdColumn}, Firstname text NOT NULL, Lastname text NOT NULL, Username text NOT NULL, Email text, Age smallint NOT NULL, ${TrackableColumns})` +
+      `CREATE TABLE Posts(${IdColumn}, ContentUrl text NOT NULL, Description varchar(200)), ${UserIdRefence}, ${TrackableColumns})` +
+      `CREATE TABLE PostLikes(${IdColumn}, ${UserIdRefence} , PostId ${UuidQuery} references Posts(Id) NOT NULL, ${TrackableColumns})` +
+      `CREATE TABLE PostComments(${IdColumn}, Text varchar(150), ParentId ${UuidQuery} references PostComments(Id), ${UserIdRefence}, PostId ${UuidQuery} references Posts(Id) NOT NULL, ${TrackableColumns})`
   );
 };
 
